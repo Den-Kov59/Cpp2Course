7ef0da3479b091d21651fd8dc20e9663f49c00a2
Den-Kov59 <78751467+Den-Kov59@users.noreply.github.com>
2021-05-24 20:57:41 +0300

    Add files via upload

---
+++ TestsServer/mainwindow.h
@@ -0,0 +1,34 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpServer>
+#include <QTcpSocket>
+#include <QTime>
+#include <QTextEdit>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QLabel>
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+private:
+    QTcpServer* m_ptcpServer;
+    QTextEdit*  m_ptxt;
+    quint16     m_nNextBlockSize;
+
+private:
+    void sendToClient(QTcpSocket* pSocket, const QString& str);
+
+public:
+    MainWindow(int nPort, QWidget* pwgt = 0);
+    ~MainWindow();
+
+public slots:
+    virtual void slotNewConnection();
+            void slotReadClient   ();
+
+
+};
+#endif // MAINWINDOW_H
